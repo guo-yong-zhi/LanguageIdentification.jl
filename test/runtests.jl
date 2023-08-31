55db921dcf0fb9ab0d9837d4e85dcfbd3859af19
guo-yong-zhi <55872791+guo-yong-zhi@users.noreply.github.com>
2023-08-31 21:56:01 +0800

    add test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,21 @@
 using LanguageIdentification
+import LanguageIdentification as LI
 using Test
 @testset "LanguageIdentification.jl" begin
-    @show LanguageIdentification.supported_languages()
-    LanguageIdentification.initialize()
-    @show LanguageIdentification.supported_languages()
-    @show langid("This is a test.")
+    @show LI.supported_languages()
+    LI.initialize()
+    @test length(LI.supported_languages()) == 50
+    @test langid("This is a test.") == "eng"
+    @test langid("这是一个测试。") == "zho"
+    @test langid("これはテストです。") == "jpn"
+    @test langid("이것은 테스트입니다.") == "kor"
+    @test langid("Это тест.") == "rus"
+    @test langid("هذا اختبار.") == "ara"
+    @test langid("यह एक परीक्षण है।") == "hin"
+    @test langid("এটি একটি পরীক্ষা।") == "ben"
+    @test langid("این یک آزمایش است.") == "fas"
+    @test sum(last.(langprob("This is a test.", topk=50))) ≈ 1.0
+    @test langprob("这是一个测试。", topk=1) |> only |> first == "zho"
+    @test langprob("これはテストです。", ["zho", "ara"], topk=30) |> length == 2
+
 end
